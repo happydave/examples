c512d00f0631b21021e525080838751b1287087d
happy <happydave@users.noreply.github.com>
2021-09-30 15:00:49 -0600

    added error for rolling the int

--- fib.go
+++ fib.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // x = fib(n-a) + fib(n-2)
 
@@ -13,11 +16,15 @@ func main() {
 	fmt.Printf("%d: %d \n", -1, fib(-1))		// -1
 	fmt.Printf("%d: %d \n", 12, fib(12))		// 144
 	fmt.Printf("%d: %d \n", 1, fib(1))			// 1
-	fmt.Printf("%d: %d \n", 65537, fib(65537))  // 4169236831028050205
 	fmt.Printf("%d: %d \n", 7, fib(7))			// 13
 	fmt.Printf("%d: %d \n", -13, fib(-13))		// -1
+	fmt.Printf("%d: %d \n", 92, fib(92))		// 7540113804746346429
+	fmt.Printf("%d: %d \n", 93, fib(93))		// -6246583658587674878 <== rolled the int
+	fmt.Printf("%d: %d \n", 95, fib(95))
+	//fmt.Printf("%d: %d \n", 65537, fib(65537))  // 4169236831028050205 is incorrect
 }
 
+// fib(index) returns fib(index-1) + fib(index-2)
 func fib(n int) int {
 
 	if n == 0 { return seq[0] }
@@ -28,6 +35,12 @@ func fib(n int) int {
 	if len(seq) < n {
 		for i := len(seq); i <= n; i++{
 			seq = append(seq, seq[i-1] + seq[i-2])
+			
+			if (seq[i-1] + seq[i-2]) < seq[i-1] {
+				// ERROR: rolled the int
+				fmt.Printf("ERROR: Int roll at %d (%d + %d returned %d) \n", i, seq[i-2], seq[i-1], seq[i])
+				os.Exit(1)
+			}
 		}
 	}
 	
